56ea9a3d7e9817754c07fc5b45def00a63009bf0
Harry Hopkinson <harryjhopkinson@gmail.com>
2022-03-07 20:46:54 +0000

    Inputs

--- src/extension.ts
+++ src/extension.ts
@@ -4,35 +4,47 @@ export function activate(context: vscode.ExtensionContext) {
 	const commandName = "fireship-extension.fireship-extension";
 
 	var command = vscode.commands.registerCommand(commandName, () => {
-		// check for highlighted text
 		const editor = vscode.window.activeTextEditor;
 		if (!editor) {
 			return vscode.window.showErrorMessage("No editor is active");
 		}
 		const selection = editor.selection;
-		if (selection.isEmpty) {
-			return vscode.window.showErrorMessage("No text selected");
-		} else {
-			// select all text
-			editor.selections = [
-				new vscode.Selection(
-					0,
-					0,
-					selection.end.line,
-					selection.end.character
-				),
-			];
-			// save the text to the clipboard but reverse it
-			// e.g the last line becomes the first line
-			const text = editor.document.getText(editor.selection);
-			vscode.env.clipboard.writeText(
-				text.split("\n").reverse().join("\n")
-			);
-			// remove the original text
-			editor.edit((editBuilder) => {
-				editBuilder.delete(selection);
+		vscode.window
+			.showInputBox({
+				placeHolder: "Enter the line numbers to remove text from",
+				prompt: "Enter the line numbers to remove text from",
+				value: selection.start.line + 1 + "-" + selection.end.line,
+			})
+			.then((value) => {
+				if (value) {
+					const lines = value.split("-");
+					const start = parseInt(lines[0]);
+					const end = parseInt(lines[1]);
+					if (lines.length !== 2 || start > end) {
+						return vscode.window.showErrorMessage(
+							"Invalid line range"
+						);
+					}
+					const text = editor.document.getText(
+						new vscode.Range(
+							new vscode.Position(start, 0),
+							new vscode.Position(end, 0)
+						)
+					);
+					vscode.env.clipboard.writeText(text);
+					editor.edit((editBuilder) => {
+						editBuilder.delete(
+							new vscode.Range(
+								new vscode.Position(start, 0),
+								new vscode.Position(end, 0)
+							)
+						);
+					});
+					vscode.window.showInformationMessage("Debugging!");
+				} else {
+					vscode.window.showErrorMessage("No text entered");
+				}
 			});
-		}
 	});
 
 	context.subscriptions.push(command);
